76e9d73813a8ce67622d1b6968e2d9605604981d
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-10-05 11:58:00 +0700

    profile page ui

---
+++ src/components/ProfilePage/Sidebar.tsx
@@ -0,0 +1,117 @@
+import clsx from "clsx";
+import { useState } from "react";
+import { HiUser, HiBellAlert } from "react-icons/hi2";
+import { FaBitcoin } from "react-icons/fa";
+import { PiNotepadFill } from "react-icons/pi";
+import { BiSolidDiscount } from "react-icons/bi";
+
+export default function Sidebar() {
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(true);
+  return (
+    <aside className="w-64 transition-transform text-sm">
+      <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50">
+        <ul className="space-y-2 font-medium">
+          <li>
+            <button
+              type="button"
+              className="flex items-center w-full px-2 pt-2 text-sm text-gray-900 transition duration-75 rounded-lg group"
+              aria-controls="dropdown-example"
+              data-collapse-toggle="dropdown-example"
+              onClick={() => setIsOpenMenu(!isOpenMenu)}
+            >
+              <HiUser className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+              <span className="flex-1 ms-3 text-left whitespace-nowrap">
+                Tài khoản
+              </span>
+            </button>
+            <ul
+              id="dropdown-example"
+              className={clsx(
+                "py-2 space-y-2 transition duration-150 font-normal text-[#000000a6]",
+                isOpenMenu && "block translate-y-0 ease-linear",
+                !isOpenMenu && "hidden -translate-y-full ease-linear"
+              )}
+            >
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center w-full text-primary transition duration-75 rounded-lg pl-11 group"
+                >
+                  Hồ sơ
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center w-full text-gray-900 transition duration-75 rounded-lg pl-11 group"
+                >
+                  Ngân hàng
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center w-full text-gray-900 transition duration-75 rounded-lg pl-11 group"
+                >
+                  Địa chỉ
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center w-full text-gray-900 transition duration-75 rounded-lg pl-11 group"
+                >
+                  Đổi mật khẩu
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center w-full text-gray-900 transition duration-75 rounded-lg pl-11 group"
+                >
+                  Cài đặt thông báo
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center px-2 py-1 text-gray-900 rounded-lg group"
+            >
+              <PiNotepadFill className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+              <span className="ms-3">Đơn mua</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center px-2 py-1 text-gray-900 rounded-lg group"
+            >
+              <HiBellAlert className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+              <span className="ms-3">Thông báo</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center px-2 py-1 text-gray-900 rounded-lg group"
+            >
+              <BiSolidDiscount className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+              <span className="ms-3">Voucher</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center px-2 py-1 text-gray-900 rounded-lg group"
+            >
+              <FaBitcoin className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+              <span className="ms-3">Tích điểm</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+}
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,125 @@
+import { Link } from "react-router-dom";
 import Container from "../components/layout/Container";
+import BreadcrumbIcon from "../components/icons/BreadcrumbIcon";
+import Sidebar from "../components/ProfilePage/Sidebar";
+import { Avatar } from "../assets";
 
 export default function ProfilePage() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <div className="w-full mt-5 flex items-center justify-start gap-2 text-sm">
+        <Link to={"/"} className="text-[#0055aa]">
+          Trang chủ
+        </Link>
+        <BreadcrumbIcon />
+        <span>Thông tin tài khoản</span>
+      </div>
+      <div className="w-full flex my-10 gap-8">
+        <Sidebar />
+        <div className="w-full flex-1 bg-white px-[30px] pb-4">
+          <div className="py-[18px] leading-6 font-medium">
+            <h2 className="capitalize text-lg text-[#333]">Hồ sơ của tôi</h2>
+            <p className="text-sm mt-1 text-[#555] font-normal">
+              Quản lý thông tin hồ sơ để bảo mật tài khoản
+            </p>
+          </div>
+          <div className="h-px w-full bg-black/10"></div>
+          <div className="pt-[30px] flex items-start">
+            <div className="flex-1 pr-[50px] w-full text-sm">
+              <form>
+                <div className="flex items-center gap-8">
+                  <label className="min-w-[20%] text-right">Email</label>
+                  <input
+                    type="text"
+                    name="email"
+                    value={"tuanngoc12340@gmail.com"}
+                    disabled
+                    className="w-full px-4 py-2 bg-gray-100 outline-none rounded-sm border cursor-not-allowed"
+                  />
+                </div>
+                <div className="flex items-center gap-8 mt-4">
+                  <label className="min-w-[20%] text-right">Họ và tên</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={"Tuan Ngoc"}
+                    className="w-full px-4 py-2 bg-white outline-none rounded-sm border"
+                  />
+                </div>
+                <div className="flex items-center gap-8 mt-4">
+                  <label className="min-w-[20%] text-right">
+                    Số điện thoại
+                  </label>
+                  <input
+                    type="text"
+                    name="phone"
+                    value={"0338597737"}
+                    className="w-full px-4 py-2 bg-white outline-none rounded-sm border"
+                  />
+                </div>
+                <div className="flex items-center gap-8 mt-4 py-2">
+                  <label className="min-w-[20%] text-right">Giới tính</label>
+                  <div className="flex items-center">
+                    <input
+                      type="radio"
+                      name="gender"
+                      value={"0338597737"}
+                      className="mr-1"
+                    />
+                    Nam
+                    <input
+                      type="radio"
+                      name="gender"
+                      value={"0338597737"}
+                      className="ms-4 mr-1"
+                    />
+                    Nữ
+                    <input
+                      type="radio"
+                      name="gender"
+                      value={"0338597737"}
+                      className="ms-4 mr-1"
+                    />
+                    Khác
+                  </div>
+                </div>
+                <div className="flex items-center gap-8 mt-4">
+                  <label className="min-w-[20%] text-right">Ngày sinh</label>
+                  <input
+                    type="date"
+                    name="birthdate"
+                    value={"16/01/2001"}
+                    className="w-full px-4 py-2 bg-white outline-none rounded-sm border"
+                  />
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                  <button
+                    type="submit"
+                    className="px-4 py-2 bg-primary text-white outline-none rounded-sm hover:opacity-80"
+                  >
+                    Lưu
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="w-[280px] flex flex-col items-center border-s border-[#efefef]">
+              <img
+                src={Avatar}
+                alt=""
+                className="rounded-full w-[100px] h-[100px] object-cover my-5"
+              />
+              <div className="flex flex-col items-center">
+                <button className="px-4 py-2 text-sm border outline-none h-10 hover:bg-gray-100 rounded-sm">
+                  Chọn Ảnh
+                </button>
+                <p className="text-[#999] text-center text-sm mt-2">
+                  Dung lượng file tối đa 1 MB <br /> Định dạng:.JPEG, .PNG
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 }
